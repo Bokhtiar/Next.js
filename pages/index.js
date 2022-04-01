535dae9972078e76da177dc982fcf73aa19d8906
bokhtiar <bokhtiartoshar1@gmail.com>
2022-04-01 14:46:02 +0600

    link route file to another file

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,11 @@
+import Link from "next/link";
+
 export default function index() {
   return (
     <div>
       home page
+      <br/>
+      <Link href='/blog'><a className="btn btn-danger">Blog</a></Link>
     </div>
   )
 }
--- pages/product/index.js
+++ pages/product/index.js
@@ -1,9 +1,11 @@
+import Link from 'next/link'
+
 export default function index() {
   return (
     <div>
-        <h4>product1</h4>
-        <h4>product2</h4>
-        <h4>product3</h4> 
+        <h4> <Link href='/product/1'> product1 </Link> </h4>
+        <h4> <Link href='/product/2'> product2 </Link> </h4>
+        <h4> <Link href='/product/3'> product3 </Link> </h4>
     </div>
   )
 }
